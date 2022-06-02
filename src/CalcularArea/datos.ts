31a240975fbc1e414633ff4fc375948bac9bd952
Juan Manuel Barria <103613099+juanbarria@users.noreply.github.com>
2022-06-02 17:07:06 -0300

    rotulos.innerHTML +

--- src/CalcularArea/datos.ts
+++ src/CalcularArea/datos.ts
@@ -1,3 +1,5 @@
+rotulo1.innerHTML = "Ingrese la base: ";
+rotulo2.innerHTML = "Ingrese la altura: ";
 let datoBase = document.getElementById("datoBase");
 let datoAltura = document.getElementById("datoAltura");
 let btnArea = document.getElementById("btnArea");
